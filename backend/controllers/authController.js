0b5a385033f3f23c1cd1e9caff37606b19f36e09
Pritam Behera <beherapritam2003@gmail.com>
2025-10-10 00:14:57 +0530

    added the update of user details feature

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,15 @@ const { Op } = require('sequelize');
 
 const registerUser = async (req, res) => {
     try {
-        let { firstName, middleName, lastName, email, contactNo, address, password } = req.body;
+        let {
+            firstName = null, 
+            middleName = null, 
+            lastName = null, 
+            email = null, 
+            contactNo = null, 
+            address = null, 
+            password = null
+        } = req.body;
         // Trim input values to remove leading/trailing spaces
         firstName = firstName ? firstName.trim() : null;
         middleName = middleName ? middleName.trim() : null;
@@ -12,28 +20,18 @@ const registerUser = async (req, res) => {
         contactNo = contactNo ? contactNo.trim() : null;
         address = address ? address.trim() : null;
         password = password ? password.trim() : null;
-        // Spaces in password are not allowed
-        if(!isValidEmail(email)) {
-            return res.status(400).json({ error: 'Invalid email format' });
-        }
-        if (password && /\s/.test(password)) {
-            return res.status(400).json({ error: 'Password should not contain spaces' });
-        }
         // Basic validation
         if (!email || !contactNo || !password) {
             return res.status(400).json({ error: 'Email, Contact Number and Password are required' });
         }
-        // Password length and strength validation
-        if (password.length < 6 || password.length > 15) {
-            return res.status(400).json({ error: 'Password must be between 6 and 15 characters' });
+        // Email format validation
+        if(!isValidEmail(email)) {
+            return res.status(400).json({ error: 'Invalid email format' });
         }
-        // At least one uppercase letter, one lowercase letter, one digit, and one special character
-        const hasUpperCase = /[A-Z]/.test(password);
-        const hasLowerCase = /[a-z]/.test(password);
-        const hasNumber = /\d/.test(password);
-        const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-        if (!hasUpperCase || !hasLowerCase || !hasNumber || !hasSpecialChar) {
-            return res.status(400).json({ error: 'Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character' });
+        // Password validation
+        const passwordValidation = PasswordTest(password);
+        if (!passwordValidation.valid) {
+            return res.status(400).json({ error: passwordValidation.message });
         }
         // Check if email or contact no already exists
         let user = await UserModel.findOne({ where: { email } });
@@ -57,14 +55,14 @@ const registerUser = async (req, res) => {
         // Return success response with created user details (excluding password)
         const userResponse = newUser.toJSON();
         delete userResponse.password;
-        res.status(201).json({ message: 'User registered successfully', userDetails: userResponse });
+        return res.status(201).json({ message: 'User registered successfully', userDetails: userResponse });
     } catch (error) {
         // Handle Sequelize validation errors
         if(error.name === 'SequelizeValidationError') {
             return res.status(400).json({ error: error.errors.map(e => e.message) });
         }
         console.error('Error registering user:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        return res.status(500).json({ error: 'Internal Server Error' });
     }
 }
 
@@ -74,14 +72,42 @@ const isValidEmail = (email) => {
     return emailRegex.test(email);
 };
 
+const PasswordTest = (password) => {
+    // Check if password is provided
+    if (!password) {
+        return { valid: false, message: 'Password is required' };
+    }
+    // Spaces in password are not allowed
+    if (/\s/.test(password)) {
+        return { valid: false, message: 'Password should not contain spaces' };
+    }
+    // Password Length validation
+    if (password.length < 6 || password.length > 15) {
+        return { valid: false, message: 'Password must be between 6 and 15 characters' };
+    }
+    // At least one uppercase letter, one lowercase letter, one digit, and one special character
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    if (!hasUpperCase || !hasLowerCase || !hasNumber || !hasSpecialChar) {
+        return { valid: false, message: 'Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character' };
+    }
+    return { valid: true };
+}
+
 const loginUser = async (req, res) => {
     try {
-        let { email, contactNo, password } = req.body;
+        let {
+            email = null, 
+            contactNo = null, 
+            password = null 
+        } = req.body;
         // Trim input values to remove leading/trailing spaces
         email = email ? email.trim() : null;
         contactNo = contactNo ? contactNo.trim() : null;
         password = password ? password.trim() : null;
-        // Passwoed should not contain spaces
+        // Password should not contain spaces
         if (password && /\s/.test(password)) {
             return res.status(400).json({ error: 'Password should not contain spaces' });
         }
@@ -118,8 +144,84 @@ const loginUser = async (req, res) => {
         }
     } catch (error) {
         console.error('Error logging in user:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        return res.status(500).json({ error: 'Internal Server Error' });
     }
 }
 
-module.exports = { registerUser, loginUser };
+const updateUserDetails = async (req, res) => {
+    try {
+        // Implementation for updating user details
+        let {
+            email = null, 
+            updatedEmail = null, 
+            firstName = null, 
+            middleName = null, 
+            lastName = null, 
+            contactNo = null, 
+            address = null, 
+            password = null
+        } = req.body;
+        // Trim input values to remove leading/trailing spaces
+        email = email ? email.trim() : null;
+        updatedEmail = updatedEmail ? updatedEmail.trim() : null;
+        firstName = firstName ? firstName.trim() : null;    
+        middleName = middleName ? middleName.trim() : null;
+        lastName = lastName ? lastName.trim() : null;
+        contactNo = contactNo ? contactNo.trim() : null;
+        address = address ? address.trim() : null;
+        password = password ? password.trim() : null;
+        // If a field is null then it will not be updated
+        // Email format validation
+        if (!email) {
+            return res.status(400).json({ error: 'Current email is required to identify user' });
+        }
+        if(!isValidEmail(email)) {
+            return res.status(400).json({ error: 'Invalid email format' });
+        }
+        // Password validation
+        if (password) {
+            const passwordValidation = PasswordTest(password);
+            if (!passwordValidation.valid) {
+                return res.status(400).json({ error: passwordValidation.message });
+            }
+        }
+        if(updatedEmail && !isValidEmail(updatedEmail)) {
+            return res.status(400).json({ error: 'Invalid updated email format' });
+        }
+        // Check if updated email or contact no already exists
+        if (updatedEmail) {
+            let user = await UserModel.findOne({ where: { email: updatedEmail } });
+            if (user) {
+                return res.status(400).json({ error: 'Updated email already exists' });
+            }
+        }
+        if (contactNo) {
+            let user = await UserModel.findOne({ where: { contact_no: contactNo } });
+            if (user) {
+                return res.status(400).json({ error: 'Contact number already exists' });
+            }
+        }
+        const userDetails = await UserModel.findOne({ where: { email } });
+        if (!userDetails) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        // Update the fields if they are provided i.e. not null
+        if (firstName !== null && firstName !== userDetails.first_name) userDetails.first_name = firstName;
+        if (middleName !== null && middleName !== userDetails.middle_name) userDetails.middle_name = middleName;
+        if (lastName !== null && lastName !== userDetails.last_name) userDetails.last_name = lastName;
+        if (updatedEmail !== null && updatedEmail !== userDetails.email) userDetails.email = updatedEmail;
+        if (contactNo !== null && contactNo !== userDetails.contact_no) userDetails.contact_no = contactNo;
+        if (address !== null && address !== userDetails.address) userDetails.address = address;
+        // Hashing of password will be done in the model hook
+        if (password !== null) userDetails.password = password;
+        await userDetails.save();
+        const updatedUserDetails = userDetails.toJSON();
+        delete updatedUserDetails.password;
+        return res.json({ message: 'User details updated successfully', user: updatedUserDetails });
+    } catch (error) {
+        console.error('Error updating user details:', error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+module.exports = { registerUser, loginUser, updateUserDetails };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser } = require('../controllers/authController');
+const { registerUser, loginUser, updateUserDetails } = require('../controllers/authController');
 
 
 router.post('/signup', registerUser);
 
 router.post('/signin', loginUser);
+
+router.put('/update', updateUserDetails);
 
 module.exports = router;
